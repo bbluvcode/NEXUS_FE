cfdee0b4ff24dacd3bf97ac4bb69fbd4ed0f4b22
unknown <bbluvcode@gmail.com>
2024-12-31 16:39:02 +0700

    yup library

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+// import { configureStore } from "@reduxjs/toolkit";
+// import productReducer from "./productSlice";
+
+// const myStore = configureStore({
+//     reducer:{
+//         products:productReducer
+//     }
+// })
+// export default myStore;
