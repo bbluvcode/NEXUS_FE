17ccb5ecc421cb2ef642c55d314a9f7462dc3de4
Ametrine <buitamman1231@gmail.com>
2025-01-02 20:39:43 +0700

    ManApiConstant20385502012025

--- src/constant/apiConstant.js
+++ src/constant/apiConstant.js
@@ -1,2 +1,2 @@
-const API_PORT = 5185;
-export const apiProduct = `http://localhost:${API_PORT}/api/Product/`;
+const API_PORT = 5185
+export const apiProduct = `http://localhost:${API_PORT}/api/Product/`
