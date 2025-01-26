9e1d1d5bf9187e6b66a34d9499d68ca897a9ae78
zzzzzzzzzzzzzzzz <huyvdg2607@gmail.com>
2025-01-26 15:07:50 +0700

    stock

--- src/views/admin/stocks/InStockOrder.js
+++ src/views/admin/stocks/InStockOrder.js
@@ -1,10 +1,93 @@
-/* eslint-disable prettier/prettier */
-import React from 'react'
+// /* eslint-disable prettier/prettier */
+// import React, { useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { fetchInStockOrders, handleSetInStockOrder } from '../../../redux/inStockOrder/inStockOrderSlice';
+// import BtnModal from '../../../components/button/BtnModal';
 
-const InStockOrder = () => {
-  return (
-    <div>InStockOrder</div>
-  )
-}
+// const InStockOrder = () => {
+//   const dispatch = useDispatch();
+//   const inStockOrders = useSelector((state) => state.inStockOrders.items);
 
-export default InStockOrder
+//   useEffect(() => {
+//     dispatch(fetchInStockOrders());
+//   }, [dispatch]);
+
+//   const formatDateSystem = (dateString) => {
+//     const date = new Date(dateString);
+//     return date.toLocaleDateString();
+//   };
+
+//   const handleEditInStockOrder = (order) => {
+//     dispatch(handleSetInStockOrder(order));
+//   };
+
+//   return (
+//     <div>
+//       <div className="d-flex justify-content-between">
+//         <h2>List of In-Stock Orders</h2>
+//         <BtnModal name="Create New Order" iform="InStockOrderCreateForm" style="primary" />
+//       </div>
+//       <div className="row">
+//         <table className="table table-hover">
+//           <thead>
+//             <tr>
+//               <th>OrderId</th>
+//               <th>RequestId</th>
+//               <th>VendorId</th>
+//               <th>EmployeeId</th>
+//               <th>StockId</th>
+//               <th>Payer</th>
+//               <th>CreateDate</th>
+//               <th>InstockDate</th>
+//               <th>PayDate</th>
+//               <th>Tax</th>
+//               <th>Total</th>
+//               <th>Currency</th>
+//               <th>isPay</th>
+//               <th></th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {inStockOrders.length > 0 ? (
+//               inStockOrders.map((order, index) => (
+//                 <tr key={index}>
+//                   <td>{order.inStockOrderId}</td>
+//                   <td>{order.inStockRequestId}</td>
+//                   <td>{order.vendorId}</td>
+//                   <td>{order.employeeId}</td>
+//                   <td>{order.stockId}</td>
+//                   <td>{order.payer}</td>
+//                   <td>{formatDateSystem(order.createDate)}</td>
+//                   <td>{formatDateSystem(order.instockDate)}</td>
+//                   <td>{formatDateSystem(order.payDate)}</td>
+//                   <td>{order.tax.toFixed(2)}</td>
+//                   <td>{order.total.toFixed(2)}</td>
+//                   <td>{order.currencyUnit}</td>
+//                   <td>{order.isPay ? 'Yes' : 'No'}</td>
+//                   <td onClick={() => handleEditInStockOrder(order)}>
+//                     <BtnModal
+//                       name={<i className="fa fa-edit"></i>}
+//                       iform="InStockOrderEditForm"
+//                       style="warning"
+//                       order={order}
+//                     />
+//                   </td>
+//                 </tr>
+//               ))
+//             ) : (
+//               <>
+//                 <tr key={'1'}>
+//                   <td colSpan="14" style={{ textAlign: 'center', color: 'red' }}>
+//                     No data available or unable to fetch data.
+//                   </td>
+//                 </tr>
+//               </>
+//             )}
+//           </tbody>
+//         </table>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default React.memo(InStockOrder);
