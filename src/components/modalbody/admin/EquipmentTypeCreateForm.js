01951030140bcc436860b2d47150a80256f04328
zzzzzzzzzzzzzzzz <huyvdg2607@gmail.com>
2025-02-01 00:04:12 +0700

    21

--- src/components/modalbody/admin/EquipmentTypeCreateForm.js
+++ src/components/modalbody/admin/EquipmentTypeCreateForm.js
@@ -1,23 +1,29 @@
 /* eslint-disable prettier/prettier */
-import React, { useState } from 'react';
+/* eslint-disable react/prop-types */
+/* eslint-disable prettier/prettier */
+import React, { useContext, useState } from 'react';
 import BtnModalCloseSubmit from '../../button/BtnModalCloseSubmit';
 import { useDispatch, useSelector } from 'react-redux';
 import { createEquipmentType, handleSetEquipmentType } from '../../../redux/equipment/equipmentTypeSlice';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
+import { DataContext } from '../../../context/DataContext';
 
-function EquipmentTypeCreateForm() {
+function EquipmentTypeCreateForm(props) {
   const dispatch = useDispatch();
+  const { setIform } = useContext(DataContext);
   const equipmentType = useSelector((state) => state.equipmentTypes.equipmentType);
+  const [formData, setFormData] = useState({
+    typeName: '',
+    provider: '',
+  });
 
-  // Validation schema using Yup
   const schema = yup.object().shape({
     typeName: yup.string().required('Type name is required'),
     provider: yup.string().required('Provider is required'),
   });
 
-  // React Hook Form setup
   const {
     register,
     handleSubmit,
@@ -29,18 +35,29 @@ function EquipmentTypeCreateForm() {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
     dispatch(handleSetEquipmentType({ ...equipmentType, [name]: value }));
   };
 
   const onSubmit = async () => {
-    dispatch(createEquipmentType(equipmentType));
+    const resultAction = await dispatch(createEquipmentType(equipmentType));
+    if (createEquipmentType.fulfilled.match(resultAction)) {
+      const equipmentTypeInfo = resultAction.payload;
+      localStorage.setItem('equipmentTypeInfo', JSON.stringify(equipmentTypeInfo));
+    }
+    if (props.client) {
+      setIform('EquipmentTypeReqCreateForm');
+      bootstrap.Modal.getInstance(document.getElementById('myModal')).show();
+    }
   };
 
   return (
     <div className="equipment-type-create-form">
       <h2 className="text-center">Create New Equipment Type</h2>
       <form onSubmit={handleSubmit(onSubmit)} className="row g-3">
-        {/* Type Name Field */}
         <div className="col-md-6">
           <label htmlFor="typeName" className="form-label">
             Type Name
@@ -56,7 +73,6 @@ function EquipmentTypeCreateForm() {
           {errors.typeName && <p className="text-danger">{errors.typeName.message}</p>}
         </div>
 
-        {/* Provider Field */}
         <div className="col-md-6">
           <label htmlFor="provider" className="form-label">
             Provider
@@ -72,7 +88,6 @@ function EquipmentTypeCreateForm() {
           {errors.provider && <p className="text-danger">{errors.provider.message}</p>}
         </div>
 
-        {/* Submit Button */}
         <div className="col-md-12">
           <BtnModalCloseSubmit />
         </div>
