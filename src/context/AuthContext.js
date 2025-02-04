e0405215836101934c5abd857625cd1d7182a612
unknown <bbluvcode@gmail.com>
2025-02-04 22:37:44 +0700

    bb

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,11 +1,17 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable prettier/prettier */
 import React, { createContext, useContext, useState, useEffect } from "react";
 import axios from "axios";
 import { apiAuth } from '../constant/apiConstant/';
+import { useDispatch } from "react-redux";
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [employee, setEmployee] = useState(null);
   const [customer, setCustomer] = useState(null);
+  // const dispatch = useDispatch()
+
+
 
   useEffect(() => {
     // Employee
@@ -49,11 +55,13 @@ export const AuthProvider = ({ children }) => {
       if (response.data && response.data.data) {
         const { token, refreshToken } = response.data.data;
         const customerInfo = decodeToken(token);
+        console.log("Decoded Token:", customerInfo);
 
         if (!customerInfo) throw new Error("Invalid token");
 
         localStorage.setItem("customerToken", token);
         localStorage.setItem("customerRefreshToken", refreshToken);
+        localStorage.setItem("customerInfo", JSON.stringify(customerInfo)); // Lưu thông tin khách hàng
         setCustomer(customerInfo);
       }
     } catch (error) {
