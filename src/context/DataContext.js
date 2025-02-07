a6d2f2bfa8002b16ab78b9a4cd50533dca253264
Ametrine <buitamman1231@gmail.com>
2025-02-07 08:45:04 +0700

    ManComplete07/02/2025

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -1,79 +1,87 @@
-
-import { useAuth } from "./AuthContext";
-import React, { createContext, useContext, useState, useEffect} from 'react'
+import { useAuth } from './AuthContext'
+import React, { createContext, useContext, useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 
 export const DataContext = createContext()
 export const useDataContext = () => {
-    return useContext(DataContext);
-};
+  return useContext(DataContext)
+}
 
 export const DataProvider = ({ children }) => {
-    
-    const { employee } = useAuth() || {};
-    const [employees, setEmployees] = useState([]);
-    const [plans, setPlans] = useState([]);
-    const [planFees, setPlanFees] = useState([]);
-    const [expandedPlans, setExpandedPlans] = useState({});
-    const [selectedPlanFee, setSelectedPlanFee] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [retailShops, setRetailShops] = useState([]);
-    const [employeeTypes, setEmployeeTypes] = useState([]);
-    const [equipments, setEquipments] = useState([]);
-    const [equipmentTypes, setEquipmentTypes] = useState([]);
-    const [stocks, setStocks] = useState([]); 
-    const [InstockOrders, setInStockOrders] = useState([]); 
-    const [iform, setIform] = useState("");
-    const [serviceSelected, setServiceSelected] = useState(null);
-    const [currentEmployee, setCurrentEmployee] = useState(() => {
-        return JSON.parse(localStorage.getItem("currentEmployee")) || null;
-    });
+  const { employee } = useAuth() || {}
+  const [employees, setEmployees] = useState([])
+  const [plans, setPlans] = useState([])
+  const [planFees, setPlanFees] = useState([])
+  const [expandedPlans, setExpandedPlans] = useState({})
+  const [selectedPlanFee, setSelectedPlanFee] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [retailShops, setRetailShops] = useState([])
+  const [employeeTypes, setEmployeeTypes] = useState([])
+  const [equipments, setEquipments] = useState([])
+  const [equipmentTypes, setEquipmentTypes] = useState([])
+  const [stocks, setStocks] = useState([])
+  const [InstockOrders, setInStockOrders] = useState([])
+  const [iform, setIform] = useState('')
+  const [serviceSelected, setServiceSelected] = useState(null)
+  const [currentEmployee, setCurrentEmployee] = useState(() => {
+    return JSON.parse(localStorage.getItem('currentEmployee')) || null
+  })
+  const [newsTitle, setNewsTitle] = useState('')
+  const [newsContent, setNewsContent] = useState('')
+  const [newsEmployeeId, setNewsEmployeeId] = useState('')
 
-    useEffect(() => {
-        if (employee) {
-            setCurrentEmployee(employee);
-            localStorage.setItem("currentEmployee", JSON.stringify(employee));
-        }
-        else{
-            setCurrentEmployee(null);
-            localStorage.removeItem("currentEmployee");
-        }
-    }, [employee]);
+  useEffect(() => {
+    if (employee) {
+      setCurrentEmployee(employee)
+      localStorage.setItem('currentEmployee', JSON.stringify(employee))
+    } else {
+      setCurrentEmployee(null)
+      localStorage.removeItem('currentEmployee')
+    }
+  }, [employee])
 
-    const data = {
-        currentEmployee,
-        employees,
-        setEmployees,
-        plans,
-        setPlans,
-        planFees,
-        setPlanFees,
-        expandedPlans,
-        setExpandedPlans,
-        selectedPlanFee,
-        setSelectedPlanFee,
-        loading,
-        setLoading,
-        retailShops,
-        setRetailShops,
-        employeeTypes,
-        setEmployeeTypes,
-        equipments,
-        setEquipments,
-        equipmentTypes,
-        setEquipmentTypes,
-        stocks,
-        setStocks,
-        InstockOrders,
-        setInStockOrders,
-        iform, setIform,
-        serviceSelected, setServiceSelected,
-    };
+  const data = {
+    currentEmployee,
+    employees,
+    setEmployees,
+    plans,
+    setPlans,
+    planFees,
+    setPlanFees,
+    expandedPlans,
+    setExpandedPlans,
+    selectedPlanFee,
+    setSelectedPlanFee,
+    loading,
+    setLoading,
+    retailShops,
+    setRetailShops,
+    employeeTypes,
+    setEmployeeTypes,
+    equipments,
+    setEquipments,
+    equipmentTypes,
+    setEquipmentTypes,
+    stocks,
+    setStocks,
+    InstockOrders,
+    setInStockOrders,
+    iform,
+    setIform,
+    serviceSelected,
+    setServiceSelected,
+    newsTitle,
+    setNewsTitle,
+    newsContent,
+    setNewsContent,
+    newsEmployeeId,
+    setNewsEmployeeId,
+  }
 
-    return <DataContext.Provider value={data}>{children}</DataContext.Provider>;
-};
+  return <DataContext.Provider value={data}>{children}</DataContext.Provider>
+}
 
 // Xác định kiểu dữ liệu cho children
 DataProvider.propTypes = {
-    children: PropTypes.node.isRequired
-};
+  children: PropTypes.node.isRequired,
+}
--- src/views/client/Services.js
+++ src/views/client/Services.js
@@ -9,15 +9,36 @@ import { getAllPlans } from '../../services/planService';
 import { getAllPlanFees } from '../../services/planFeeService';
 import { useDataContext } from '../../context/DataContext';
 import { Pagination } from 'react-bootstrap';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+const API_BASE_URL = 'http://localhost:5185/api/news'
 
 function Services() {
+  const navigate = useNavigate();
   const { plans, setPlans, planFees, setPlanFees, loading, setLoading } = useDataContext();
+  const [newsList, setNewsList] = useState([])
   const [currentPage, setCurrentPage] = useState({});
 
+  useEffect(() => {
+    const fetchNews = async () => {
+      try {
+        const newsResponse = await axios.get(API_BASE_URL)
+        setNewsList(newsResponse.data)
+      } catch (error) {
+        console.error('Error fetching news', error)
+      }
+    }
+
+    fetchNews()
+  }, [])
   useEffect(() => {
     Promise.all([getAllPlans(), getAllPlanFees()])
       .then(([plansData, planFeesData]) => {
         setPlans(plansData);
+        console.log(plansData);
+        console.log(planFeesData);
+
+
         setPlanFees(planFeesData);
         setLoading(false);
       })
@@ -27,6 +48,16 @@ function Services() {
       });
   }, [setPlans, setPlanFees, setLoading]);
 
+  const extractFirstImage = (htmlContent) => {
+    const doc = new DOMParser().parseFromString(htmlContent, 'text/html')
+    const image = doc.querySelector('img')
+    return image ? image.src : ''
+  }
+
+  // Hàm để chuyển hướng tới trang chi tiết bài viết
+  const handleDetail = (newsId) => {
+    navigate(`/newsDetail/${newsId}`)
+  }
   const handlePageChange = (planId, pageNumber) => {
     setCurrentPage((prev) => ({ ...prev, [planId]: pageNumber }));
   };
@@ -39,7 +70,7 @@ function Services() {
     <div className="container mt-5 mb-5">
       {/* Carousel Section */}
       <Carousel
-        className={styles.carouselContainer}
+        className="carouselContainer"
         showThumbs={false}
         infiniteLoop
         useKeyboardArrows
@@ -48,147 +79,171 @@ function Services() {
         centerMode
         centerSlidePercentage={100}
       >
-        {plans.slice(0, 3).map((item) => (
-          <div key={item.planId} className="card p-3 text-center">
-            <div className="card-body">
-              <h4 className="card-title">{item.planName}</h4>
-              <p className="card-text">{item.description}</p>
-              <p className="card-text">
-                Security Deposit: <strong>${item.securityDeposit.toFixed(2)}</strong>
-              </p>
-              <p className="card-text">
-                Status: <strong>{item.isUsing ? 'Active' : 'Inactive'}</strong>
-              </p>
-              {/* View button for the Card View */}
-              <button
-                className="btn btn-info btn-sm"
-                onClick={() => alert(`View details for ${item.planName}`)}
+        {newsList
+          .filter((news) => news.status === true)
+          .map((news) => {
+            const firstImage = extractFirstImage(news.content)
+
+            return (
+              <div
+                key={news.newsId}
+                className="card p-3 text-center"
+                onClick={() => handleDetail(news.newsId)}
+                style={{ cursor: 'pointer' }}
               >
-                View
-              </button>
-            </div>
-          </div>
-        ))}
+                <div className="card-body">
+                  {/* Hiển thị hình ảnh đầu tiên */}
+                  {firstImage ? (
+                    <img
+                      src={firstImage}
+                      alt={`news-image-${news.newsId}`}
+                      className="card-img-top"
+                      style={{
+                        width: '100%',    // Chiều rộng cố định là 100% của phần tử cha
+                        height: '200px',  // Chiều cao cố định là 200px
+                        objectFit: 'contain', // Giữ nguyên tỷ lệ ảnh, thu nhỏ lại nếu quá lớn
+                      }}
+                    />
+                  ) : (
+                    <div
+                      className="card-img-top d-flex align-items-center justify-content-center"
+                      style={{
+                        height: '200px',
+                        backgroundColor: '#f0f0f0',
+                        color: '#333',
+                      }}
+                    >
+                      <p className="text-center">No Image</p>
+                    </div>
+                  )}
+                  <h4 className="card-title">{news.title}</h4>
+                  <p className="card-text">{news.description}</p>
+                </div>
+              </div>
+            )
+          })}
       </Carousel>
 
+
       <div className="mt-5 d-flex flex-column">
-        {plans.map((plan) => {
-          const planFeeList = planFees.filter((fee) => fee.planId === plan.planId);
-          const itemsPerPage = 5;
-          const totalPages = Math.ceil(planFeeList.length / itemsPerPage);
-          const currentPageIndex = currentPage[plan.planId] || 1;
-          const displayedFees = planFeeList.slice((currentPageIndex - 1) * itemsPerPage, currentPageIndex * itemsPerPage);
+        {plans
+          .filter((plan) => plan.isUsing === true).map((plan) => {
+            const planFeeList = planFees.filter((fee) => fee.planId === plan.planId);
+            const itemsPerPage = 5;
+            const totalPages = Math.ceil(planFeeList.length / itemsPerPage);
+            const currentPageIndex = currentPage[plan.planId] || 1;
+            const displayedFees = planFeeList.slice((currentPageIndex - 1) * itemsPerPage, currentPageIndex * itemsPerPage);
 
-          return (
-            <div key={plan.planId} className="mb-5">
-              <h4 className="card-title">{plan.planName}</h4>
+            return (
+              <div key={plan.planId} className="mb-5">
+                <h4 className="card-title">{plan.planName}</h4>
 
-              {planFeeList.length <= 5 ? (
-                <div className="container" style={{ maxWidth: '1200px' }}>
-                  <Carousel
-                    style={{
-                      width: '100%',
-                      margin: '0 auto',
-                      paddingLeft: '0',
-                      position: 'relative',
-                      zIndex: 0,
-                    }}
-                    showThumbs={false}
-                    infiniteLoop
-                    useKeyboardArrows
-                    showStatus={false}
-                    showIndicators={false}
-                    centerMode
-                    centerSlidePercentage={33.33} // Show 3 items at once
-                  >
-                    {planFeeList.map((fee) => (
-                      <div
-                        key={fee.planFeeId}
-                        style={{
-                          width: '70%',
-                          marginRight: 'auto',
-                          marginLeft: 'auto',
-                          marginTop: '20px',
-                          marginBottom: '20px',
-                          backgroundColor: 'white',
-                          boxSizing: 'border-box',
-                          transition: 'transform 0.3s ease-in-out',
-                          textAlign: 'center',
-                          display: 'flex',
-                          flexDirection: 'column',
-                          justifyContent: 'space-between',
-                          borderRadius: '8px',
-                          border: '1px solid rgba(0, 0, 0, 0.125)',
-                          boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
-                          padding: '20px',
-                          position: 'relative',
-                          paddingBottom: '30px',  // Add extra padding to the bottom to prevent cutting off content
-                        }}
-                      >
-                        <div className="card-body">
-                          <p>
-                            <strong>{fee.planFeeName}:</strong> ${fee.rental.toFixed(2)}
-                          </p>
-                          <p>{fee.description}</p>
-                          <button
-                            className="btn btn-info btn-sm"
-                            onClick={() => alert(`View details for ${fee.planFeeName}`)}
-                          >
-                            View
-                          </button>
-                        </div>
-                      </div>
-                    ))}
-                  </Carousel>
-                </div>
-              ) : (
-                <div style={{ overflowX: 'auto', minHeight: '350px' }}>
-                  <table className="table table-hover table-bordered mt-3" style={{ tableLayout: 'fixed' }}>
-                    <thead>
-                      <tr>
-                        <th>Plan Fee Name</th>
-                        <th>Rental</th>
-                        <th>Description</th>
-                        <th>Action</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {displayedFees.map((fee) => (
-                        <tr key={fee.planFeeId}>
-                          <td>{fee.planFeeName}</td>
-                          <td>${fee.rental.toFixed(2)}</td>
-                          <td>{fee.description}</td>
-                          <td>
-                            <button
-                              className="btn btn-primary btn-sm"
-                              onClick={() => alert(`Register for ${fee.planFeeName}`)}
-                            >
-                              Register
-                            </button>
-                          </td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
-              )}
-
-              {planFeeList.length > 5 && (
-                <Pagination className="justify-content-center">
-                  {[...Array(totalPages)].map((_, index) => (
-                    <Pagination.Item
-                      key={index + 1}
-                      active={index + 1 === currentPageIndex}
-                      onClick={() => handlePageChange(plan.planId, index + 1)}
+                {planFeeList.length <= 5 ? (
+                  <div className="container" style={{ maxWidth: '1200px' }}>
+                    <Carousel
+                      style={{
+                        width: '100%',
+                        margin: '0 auto',
+                        paddingLeft: '0',
+                        position: 'relative',
+                        zIndex: 0,
+                      }}
+                      showThumbs={false}
+                      infiniteLoop
+                      useKeyboardArrows
+                      showStatus={false}
+                      showIndicators={false}
+                      centerMode
+                      centerSlidePercentage={33.33} // Show 3 items at once
                     >
-                      {index + 1}
-                    </Pagination.Item>
-                  ))}
-                </Pagination>
-              )}
-            </div>
-          );
-        })}
+                      {planFeeList.map((fee) => (
+                        <div
+                          key={fee.planFeeId}
+                          style={{
+                            width: '70%',
+                            marginRight: 'auto',
+                            marginLeft: 'auto',
+                            marginTop: '20px',
+                            marginBottom: '20px',
+                            backgroundColor: 'white',
+                            boxSizing: 'border-box',
+                            transition: 'transform 0.3s ease-in-out',
+                            textAlign: 'center',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'space-between',
+                            borderRadius: '8px',
+                            border: '1px solid rgba(0, 0, 0, 0.125)',
+                            boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+                            padding: '20px',
+                            position: 'relative',
+                            paddingBottom: '30px',  // Add extra padding to the bottom to prevent cutting off content
+                          }}
+                        >
+                          <div className="card-body">
+                            <p>
+                              <strong>{fee.planFeeName}:</strong> ${fee.rental.toFixed(2)}
+                            </p>
+                            <p>{fee.description}</p>
+                            <button
+                              className="btn btn-info btn-sm"
+                              onClick={() => alert(`View details for ${fee.planFeeName}`)}
+                            >
+                              View
+                            </button>
+                          </div>
+                        </div>
+                      ))}
+                    </Carousel>
+                  </div>
+                ) : (
+                  <div style={{ overflowX: 'auto', minHeight: '350px' }}>
+                    <table className="table table-hover table-bordered mt-3" style={{ tableLayout: 'fixed' }}>
+                      <thead>
+                        <tr>
+                          <th>Plan Fee Name</th>
+                          <th>Rental</th>
+                          <th>Description</th>
+                          <th>Action</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {displayedFees.map((fee) => (
+                          <tr key={fee.planFeeId}>
+                            <td>{fee.planFeeName}</td>
+                            <td>${fee.rental.toFixed(2)}</td>
+                            <td>{fee.description}</td>
+                            <td>
+                              <button
+                                className="btn btn-primary btn-sm"
+                                onClick={() => alert(`Register for ${fee.planFeeName}`)}
+                              >
+                                Register
+                              </button>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </div>
+                )}
+
+                {planFeeList.length > 5 && (
+                  <Pagination className="justify-content-center">
+                    {[...Array(totalPages)].map((_, index) => (
+                      <Pagination.Item
+                        key={index + 1}
+                        active={index + 1 === currentPageIndex}
+                        onClick={() => handlePageChange(plan.planId, index + 1)}
+                      >
+                        {index + 1}
+                      </Pagination.Item>
+                    ))}
+                  </Pagination>
+                )}
+              </div>
+            );
+          })}
       </div>
     </div>
   );
